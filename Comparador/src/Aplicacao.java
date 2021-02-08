b527d075cc1a0d8bed21bf94df43c60b04ba011c
marinabotti <marina.rbotti.adv@gmail.com>
2021-02-08 14:46:18 -0300

    adcionando exercicio comparador

---
+++ Comparador/src/Aplicacao.java
@@ -0,0 +1,12 @@
+public class Aplicacao {
+
+    public static void main(String[] args){
+
+        Comparador comparador = new Comparador();
+
+        boolean comparacao = comparador.menorQue(15,20);
+
+    System.out.println(comparacao);
+}
+
+}
---
+++ Comparador/src/Comparador.java
@@ -0,0 +1,8 @@
+public class Comparador {
+
+    public boolean menorQue(int numero1, int numero2){
+
+        boolean menorQue = numero1 < numero2;
+        return  menorQue;
+    }
+}
